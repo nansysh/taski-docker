528d6e821e0735ad0db81345bfe4dd06a609080e
Anastasia Shchepanskaya <anastasia.shchepanskaya@gmail.com>
2023-07-10 21:52:33 +0300

    According to  PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
-  
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -11,9 +11,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 if DEBUG:
     load_dotenv()
-    
+
 ALLOWED_HOSTS = ['localhost', '84.252.139.0', '127.0.0.1', 'nansyshtaski.sytes.net']
-# ALLOWED_HOSTS = []
 
 # Application definition
 
